a22c580e992840948941f0a8a726cfc0146c3362
Yeshwanth Reddy Peddola <146018022+Yeshwanth1281@users.noreply.github.com>
2024-04-15 12:59:30 -0400

    Update LAB 2.R

--- LAB 2.R
+++ LAB 2.R
@@ -1,26 +1,26 @@
-# Load necessary libraries
+# Loading the required libraries
 library(tidyverse)
 library(caret)
 library(glmnet)
 
-# Set working directory
+# Setting of the work directory
 setwd("C:/Users/peddo/Downloads/")
 
-# Read the datasets
+# Reading the loaded datasets
 students <- read.csv("students.csv")
 assessments <- read.csv("assessments.csv")
 
-# Merge datasets
+# Merging the datasets students and assessments
 merge_data <- merge(students, assessments, by = c("id_student", "code_module", "code_presentation"))
 
-# Remove rows with missing values
+# Removing the rows with missing or null values
 merge_data <- merge_data %>% drop_na()
 
 # Convert date columns to Date type
 date_columns <- c("date", "date_submitted")
 merge_data[date_columns] <- lapply(merge_data[date_columns], as.Date)
 
-# Convert categorical columns to factors
+# Convertion of the categorical columns to factors
 categorical_columns <- c("gender", "region", "highest_education", "imd_band", "age_band", "final_result", "assessment_type")
 merge_data[categorical_columns] <- lapply(merge_data[categorical_columns], as.factor)
 
@@ -35,7 +35,7 @@ test_data <- merge_data[-trainIndex, ]
 clf_formula <- final_result ~ . - id_student - date - date_registration - date_unregistration
 clf_model <- glm(classification_formula, data = train_data, family = binomial)
 
-# Predict on test data
+# Prediction on test data
 predictions_clf <- predict(clf_model, newdata = test_data, type = "response")
 
 # reg Model (predicting score)
@@ -43,10 +43,9 @@ predictions_clf <- predict(clf_model, newdata = test_data, type = "response")
 reg_formula <- score ~ . - id_student - date - date_registration - date_unregistration
 reg_model <- glm(reg_formula, data = train_data)
 
-# Predict on test data
+# Prediction on test data
 predictions_reg <- predict(reg_model, newdata = test_data)
 
-# Model interpretation
 # Classification model summary
 summary(clf_model)
 
@@ -58,4 +57,7 @@ summary(reg_model)
 
 #Tidy up the regression Summary
 tidy(reg_model)
+
+#Glance the regression summary
+glance(reg_model)
 
